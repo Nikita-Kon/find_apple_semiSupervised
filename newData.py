e9a27b3464c680273f525899a937a1883a3f439a
Nikita-Kon <nn8475537@gmail.com>
2024-08-04 00:51:34 +0300

    new logic to newDatta separates folders for Pseudodata and images without annotation

--- newData.py
+++ newData.py
@@ -17,33 +17,39 @@ def min_max_normalize(data):
     max_val = np.max(data, axis=0)
     return (data - min_val) / (max_val - min_val)
 
-# load model
-model = YOLO(r"C:\Users\Aser\PycharmProjects\torch-yolo\find_apple_semiSupervised\runs\train\exp\weights\best.pt")
-# read pictures
-source_directory = r"C:\Users\Aser\PycharmProjects\torch-yolo\find_apple_semiSupervised\apples_learning\trainUnlabeled\images"
-image_paths = get_image_paths(source_directory)
+def makePseudoLabels():
+    # load model
+    model = YOLO(r"best.pt")
+    # read pictures
+    source_directory = r"apples_learning/unlabeled_images/images"
+    image_paths = get_image_paths(source_directory)
 
-for i in image_paths:
-    image = cv2.imread(i)
-    # predict
-    result = model.predict(source=image)
+    for i in image_paths:
+        image = cv2.imread(i)
+        # predict
+        result = model.predict(source=image)
 
-    boxes = result[0].boxes
-    for box in boxes:
-        x1, y1, x2, y2 = map(int, box.xyxy[0])
-        confidence = box.conf[0]
-        class_id = box.cls[0]
-        class_name = model.names[int(class_id)]
-        if confidence > 0.7:
-            data = [x1, y1, x2, y2, 0, 256]
-            data = min_max_normalize(data)
-            x1, y1, x2, y2 = data[:4]
-            # Extract the filename without the directory path
-            filename_with_extension = os.path.basename(i)
-            # Replace the .jpg extension with .txt
-            filename_txt = os.path.splitext(filename_with_extension)[0] + ".txt"
-            path = r"C:\Users\Aser\PycharmProjects\torch-yolo\find_apple_semiSupervised\apples_learning\trainUnlabeled\labels" + '\\' + filename_txt
-            print(path)
-            with open(path, 'a') as file:
-                res_string = f"0 {x1} {y1} {x2} {y1} {x2} {y2} {x1} {y2}"
-                file.write(res_string + '\n')
+        boxes = result[0].boxes
+        for box in boxes:
+            x1, y1, x2, y2 = map(int, box.xyxy[0])
+            confidence = box.conf[0]
+            class_id = box.cls[0]
+            class_name = model.names[int(class_id)]
+            if confidence > 0.7:
+                data = [x1, y1, x2, y2, 0, 256]
+                data = min_max_normalize(data)
+                x1, y1, x2, y2 = data[:4]
+                # Extract the filename without the directory path
+                filename_with_extension = os.path.basename(i)
+                # Replace the .jpg extension with .txt
+                filename_txt = os.path.splitext(filename_with_extension)[0] + ".txt"
+                path = r"C:\Users\Aser\PycharmProjects\torch-yolo\find_apple_semiSupervised\apples_learning\pseudoLabels\labels" + '\\' + filename_txt
+                print(path)
+                # change choosen image dir
+                image_destination = r"C:\Users\Aser\PycharmProjects\torch-yolo\find_apple_semiSupervised\apples_learning\pseudoLabels\images" + '\\' + filename_with_extension
+                os.rename(i, image_destination)
+                with open(path, 'a') as file:
+                    res_string = f"0 {x1} {y1} {x2} {y1} {x2} {y2} {x1} {y2}"
+                    file.write(res_string + '\n')
+
+makePseudoLabels()
--- train_on_unlabeled_data.py
+++ train_on_unlabeled_data.py
@@ -1,14 +1,20 @@
 from ultralytics import YOLO
 import os
+
+def changeModelDir():
+    trained_model_path = "runs/detect/train/weights/best.pt"
+    destination = "best.pt"
+    if os.path.exists(destination):
+        os.remove(destination)
+    if os.path.exists(trained_model_path):
+        os.rename(trained_model_path, destination)
+
 if __name__ == "__main__":
 
-    # Load the previously trained YOLO model
-    model = YOLO("runs/train/exp/weights/best.pt")
+    # # Load the previously trained YOLO model
+    # model = YOLO("runs/train/exp/weights/best.pt")
+    #
+    # # Continue training with the new dataset
+    # model.train(data="data_unlabeled.yaml", epochs=10, imgsz=256, device=0)
 
-    # Continue training with the new dataset
-    model.train(data="data_unlabeled.yaml", epochs=10, imgsz=256, device=0)
-
-    temp_model_path = "runs/detect/train/weights/best.pt"
-    final_model_path = "best.pt"
-    if os.path.exists(temp_model_path):
-        os.rename(temp_model_path, final_model_path)
+    changeModelDir()
